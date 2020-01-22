9ef7be7c78adc33a7f6ab46bff57618eadfbb75b
rarmiger <rarmiger@gmail.com>
2020-01-22 18:41:23 -0500

    first cyber_glove serial server and streaming client pair

--- python/minivie/inputs/cyber_glove.py
+++ python/minivie/inputs/cyber_glove.py
@@ -2,6 +2,8 @@
 """
 communicate between cyberglove bluetooth serial interface and UDP
 
+sudo python3 -m inputs.cyber_glove
+
 @author: Armiger
 """
 import serial
@@ -14,7 +16,7 @@ def main():
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # Enable broadcasting
     sock.settimeout(1.0)
 
-    ser = serial.Serial("/dev/rfcomm0", 115200, timeout=1.0)
+    ser = serial.Serial("/dev/rfcomm2", 115200, timeout=1.0)
     #   ser.open()
     ser.write(b'?W')
     r = ser.read(100)
@@ -22,18 +24,22 @@ def main():
     ser.write(b'?S')
     r = ser.read(100)
     print(r)
+    print('Starting Streaming')
+    ser.write(b'S')
+
     while 1:
-        print('Getting Data')
-        ser.write(b'G')
+        # print('Getting Data')
+        # ser.write(b'G')
         buff = b''
 
         # Read full response
         while 1:
             r = ser.read()
             if r == b'\x00':
-                print(f'Got NULL terminator Msg={buff}')
+                print(f'Msg [{len(buff)}] = {buff}')
                 break
             buff += r
+        sock.sendto(buff, ('localhost', 16700))
 
 
 if __name__ == '__main__':
